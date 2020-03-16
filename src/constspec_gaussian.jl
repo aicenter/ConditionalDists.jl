443bb47f23fdeb02464d7f079ca163ee34e6653a
Niklas Heim <heim.niklas@gmail.com>
2020-03-16 15:07:03 +0100

    const/spec gaussian interface update

--- src/constspec_gaussian.jl
+++ src/constspec_gaussian.jl
@@ -1,6 +1,6 @@
-export loglikelihood
-export const_mean, const_variance, const_mean_var
-export spec_mean, spec_variance, spec_mean_var
+export logpdf
+export const_mean, const_var, const_mean_var
+export spec_mean, spec_var, spec_mean_var
 export ConstSpecGaussian
 
 struct ConstSpecGaussian{C<:AbstractPDF,S<:AbstractCPDF} <: AbstractCPDF
@@ -12,13 +12,27 @@ ConstSpecGaussian(c::C, s::S) where {C,S} = ConstSpecGaussian{C,S}(c,s)
 
 Flux.@functor ConstSpecGaussian
 
+function eltype(p::ConstSpecGaussian)
+    T = eltype(p.cnst)
+    @assert T == eltype(p.spec)
+    return T
+end
+
+function length(p::ConstSpecGaussian)
+    l = length(p.cnst)
+    @assert l == length(p.spec)
+    return l
+end
+
 const_mean(p::ConstSpecGaussian) = mean(p.cnst)
-const_variance(p::ConstSpecGaussian) = variance(p.cnst)
+const_var(p::ConstSpecGaussian) = var(p.cnst)
+const_cov(p::ConstSpecGaussian) = Diagonal(const_var(p))
 const_mean_var(p::ConstSpecGaussian) = mean_var(p.cnst)
 const_rand(p::ConstSpecGaussian) = rand(p.cnst)
 
 spec_mean(p::ConstSpecGaussian, z::AbstractArray) = mean(p.spec, z)
-spec_variance(p::ConstSpecGaussian, z::AbstractArray) = variance(p.spec, z)
+spec_var(p::ConstSpecGaussian, z::AbstractArray) = var(p.spec, z)
+spec_cov(p::ConstSpecGaussian) = Diagonal(spec_var(p))
 spec_mean_var(p::ConstSpecGaussian, z::AbstractArray) = mean_var(p.spec, z)
 spec_rand(p::ConstSpecGaussian, z::AbstractArray) = rand(p.spec, z)
 
@@ -28,19 +42,19 @@ function mean(p::ConstSpecGaussian, z::AbstractArray)
     (μc, μs)
 end
 
-function variance(p::ConstSpecGaussian, z::AbstractArray)
-    σc = repeat(const_variance(p), 1, size(z,2))
-    σs = spec_variance(p, z)
+function var(p::ConstSpecGaussian, z::AbstractArray)
+    σc = repeat(const_var(p), 1, size(z,2))
+    σs = spec_var(p, z)
     (σc, σs)
 end
 
-mean_var(p::ConstSpecGaussian, z::AbstractArray) = (mean(p,z), variance(p,z))
+mean_var(p::ConstSpecGaussian, z::AbstractArray) = (mean(p,z), var(p,z))
 
 rand(p::ConstSpecGaussian, z::AbstractArray) = (const_rand(p), spec_rand(p,z))
 
-function loglikelihood(p::ConstSpecGaussian, x::AbstractArray, z::AbstractArray)
-    cllh = loglikelihood(p.cnst, x)
-    sllh = loglikelihood(p.spec, x, z)
+function logpdf(p::ConstSpecGaussian, x::AbstractArray, z::AbstractArray)
+    cllh = logpdf(p.cnst, x)
+    sllh = logpdf(p.spec, x, z)
     cllh + sllh
 end
 
