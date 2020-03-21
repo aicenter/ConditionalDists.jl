8dcf637c0a1264b9c5b6af15a5ab9127d16d7310
Niklas Heim <niklas.heim@aic.fel.cvut.cz>
2020-03-21 13:15:51 +0100

    add doc strings

--- src/abstract_cgaussian.jl
+++ src/abstract_cgaussian.jl
@@ -10,7 +10,70 @@ struct DiagVar <: AbstractVar end
 """Scalar variance represented as a one-element vector"""
 struct ScalarVar <: AbstractVar end
 
-function rand(p::ACGaussian, z::AbstractArray)
+"""
+    mean_var(p::Gaussian)
+
+Returns mean and variance of a Gaussian.
+"""
+mean_var(p::Gaussian)
+
+"""
+    mean(p::Gaussian)
+
+Returns mean of a Gaussian.
+"""
+mean(p::Gaussian)
+
+"""
+    var(p::Gaussian)
+
+Returns variance of a Gaussian.
+"""
+var(p::Gaussian)
+
+"""
+    rand(p::Gaussian; batch=1)
+
+Produce `batch` number of samples from a Gaussian.
+"""
+rand(p::Gaussian, batchsize::Int=1)
+
+"""
+    logpdf(p::Gaussian, x::AbstractArray)
+
+Computes log p(x|μ,σ2).
+"""
+logpdf(p::Gaussian, x::AbstractArray)
+
+
+"""
+    mean_var(p::AbstractConditionalGaussian, z::AbstractArray)
+
+Returns mean and variance of a conditional Gaussian.
+"""
+mean_var(p::AbstractConditionalGaussian, z::AbstractArray)
+
+
+"""
+    mean(p::AbstractConditionalGaussian, z::AbstractArray)
+
+Returns mean of a conditional Gaussian.
+"""
+mean(p::AbstractConditionalGaussian, z::AbstractArray)
+
+"""
+    var(p::AbstractConditionalGaussian, z::AbstractArray)
+
+Returns variance of a conditional Gaussian.
+"""
+var(p::AbstractConditionalGaussian, z::AbstractArray)
+
+"""
+    rand(p::AbstractConditionalGaussian, z::AbstractArray)
+
+Produce `batch` number of samples from a conditional Gaussian.
+"""
+function rand(p::AbstractConditionalGaussian, z::AbstractArray)
     (μ, σ2) = mean_var(p, z)
     r = randn!(similar(μ))
     μ .+ sqrt.(σ2) .* r 
@@ -24,5 +87,10 @@ function _logpdf(p::ACGaussian, x::AbstractArray, z::AbstractArray)
     -sum(y, dims=1) / 2
 end
 
+"""
+    logpdf(p::AbstractConditionalGaussian, x::AbstractArray, z::AbstractArray)
+
+Computes log p(x|z).
+"""
 logpdf(p::ACGaussian, x::AbstractVector, z::AbstractVector) = _logpdf(p,x,z)
 logpdf(p::ACGaussian, X::AbstractMatrix, Z::AbstractMatrix) = _logpdf(p,X,Z)
