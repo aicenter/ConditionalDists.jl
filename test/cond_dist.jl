0d2bb5500dae50ce1b962244a66ddfe74b1970d7
Niklas Heim <heim.niklas@gmail.com>
2020-07-30 18:25:12 +0200

    test general cond dist

---
+++ test/cond_dist.jl
@@ -0,0 +1,43 @@
+@testset "ConditionalDistribution" begin
+
+    xlength = 3
+    zlength = 2
+    batchsize = 10
+    m = SplitLayer(zlength, [xlength,xlength])
+    d = TuringMvNormal
+    p = ConditionalDistribution(d,m) |> gpu
+
+    # MvNormal
+    res = condition(p, rand(zlength) |> gpu)
+    μ = mean(res)
+    σ2 = var(res)
+    @test res isa TuringDiagMvNormal
+    @test size(μ) == (xlength,)
+    @test size(σ2) == (xlength,)
+
+    x = rand(Float32, xlength) |> gpu
+    z = rand(Float32, zlength) |> gpu
+    loss() = logpdf(p,x,z)
+    ps = Flux.params(p)
+    @test_broken loss() isa Float32
+    @test_nowarn Flux.gradient(loss, ps)
+
+    f() = sum(rand(p,z))
+    @test_nowarn Flux.gradient(f, ps)
+
+    # Array of DiagMvNormal
+    res = condition(p, rand(zlength,batchsize)|>gpu)
+    @test res isa DistributionsAD.VectorOfMultivariate
+
+    x = rand(Float32, xlength, batchsize) |> gpu
+    z = rand(Float32, zlength, batchsize) |> gpu
+    loss() = sum(logpdf(p,x,z))
+    ps = Flux.params(p)
+    @test length(ps) == 4
+    @test_broken loss() isa Float32
+    @test_nowarn gs = Flux.gradient(loss, ps)
+
+    f() = sum(rand(p,z))
+    @test_nowarn Flux.gradient(f, ps)
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -13,7 +13,20 @@ Flux.@functor ConditionalDists.BatchDiagMvNormal
 Flux.@functor ConditionalDists.BatchScalMvNormal
 Flux.@functor DistributionsAD.TuringDiagMvNormal
 Flux.@functor DistributionsAD.TuringScalMvNormal
+Flux.@functor ConditionalDistribution
 Flux.@functor ConditionalMvNormal
 
+struct SplitLayer
+    layers::Tuple
+end
+function SplitLayer(input::Int, outputs::Array{Int,1}, act=abs)
+    SplitLayer(Tuple(Dense(input,out,act) for out in outputs))
+end
+function (m::SplitLayer)(x)
+    Tuple(layer(x) for layer in m.layers)
+end
+Flux.@functor SplitLayer
+
+include("cond_dist.jl")
 include("batch_mvnormal.jl")
 include("cond_mvnormal.jl")
