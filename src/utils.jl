38d003036dcfc7a46433f804d98a493b795ff1e1
Niklas Heim <heim.niklas@gmail.com>
2020-10-26 21:26:08 +0100

    shared variance

--- src/utils.jl
+++ src/utils.jl
@@ -2,13 +2,36 @@ struct SplitLayer{T<:Tuple}
     layers::T
 end
 
-SplitLayer(xs...) = SplitLayer(xs)
+SplitLayer(xs...) = SplitLayer(map(maybe_trainable, xs))
 
 function (m::SplitLayer)(x)
     Tuple(layer(x) for layer in m.layers)
 end
 
 @functor SplitLayer
+
+
+# for use as e.g. shared variance
+struct TrainableVector{T<:AbstractArray}
+    v::T
+end
+(v::TrainableVector)(x::AbstractVector) = v.v
+(v::TrainableVector)(x::AbstractMatrix) = v.v .* reshape(fillsimilar(v.v,size(x,ndims(x)),1),1,:)
+(v::TrainableVector)() = v.v
+@functor TrainableVector
+
+# for use as e.g. shared variance
+struct TrainableScalar{T<:Real}
+    s::AbstractVector{T}
+end
+TrainableScalar(x::Real) = TrainableScalar([x])
+(s::TrainableScalar)(x::AbstractVector) = s.s[1]
+(s::TrainableScalar)(x::AbstractMatrix) = fillsimilar(x,size(x,ndims(x)),s.s[1])
+@functor TrainableScalar
+
+maybe_trainable(x) = x
+maybe_trainable(x::AbstractArray) = TrainableVector(x)
+maybe_trainable(x::Real) = TrainableScalar(x)
 
 fillsimilar(x::AbstractArray, s::Tuple, value::Real) = fill!(similar(x, s...), value)
 fillsimilar(x::AbstractArray, s, value::Real) = fill!(similar(x, s), value)
--- test/cond_mvnormal.jl
+++ test/cond_mvnormal.jl
@@ -5,17 +5,19 @@
     batchsize = 10
     m = SplitLayer(zlength, [xlength,xlength], [identity,abs])
     p = ConditionalMvNormal(m) |> gpu
+    x = rand(Float32, xlength) |> gpu
+    z = rand(Float32, zlength) |> gpu
+    X = rand(Float32, xlength, batchsize) |> gpu
+    Z = rand(Float32, zlength, batchsize) |> gpu
 
     # MvNormal
-    res = condition(p, rand(zlength) |> gpu)
+    res = condition(p,z)
     μ = mean(res)
     σ2 = var(res)
     @test res isa TuringDiagMvNormal
     @test size(μ) == (xlength,)
     @test size(σ2) == (xlength,)
 
-    x = rand(Float32, xlength) |> gpu
-    z = rand(Float32, zlength) |> gpu
     loss() = logpdf(p,x,z)
     ps = Flux.params(p)
     @test_broken loss() isa Float32
@@ -25,16 +27,14 @@
     @test_broken Flux.gradient(f, ps)
 
     # BatchDiagMvNormal
-    res = condition(p, rand(zlength,batchsize)|>gpu)
+    res = condition(p,Z)
     μ = mean(res)
     σ2 = var(res)
     @test res isa ConditionalDists.BatchDiagMvNormal
     @test size(μ) == (xlength,batchsize)
     @test size(σ2) == (xlength,batchsize)
 
-    x = rand(Float32, xlength, batchsize) |> gpu
-    z = rand(Float32, zlength, batchsize) |> gpu
-    loss() = sum(logpdf(p,x,z))
+    loss() = sum(logpdf(p,X,Z))
     ps = Flux.params(p)
     @test length(ps) == 4
     @test loss() isa Float32
@@ -49,16 +49,14 @@
     d = MvNormal(zeros(Float32,xlength), 1f0)
     p = ConditionalMvNormal(m) |> gpu
 
-    res = condition(p, rand(zlength,batchsize)|>gpu)
+    res = condition(p,Z)
     μ = mean(res)
     σ2 = var(res)
     @test res isa ConditionalDists.BatchScalMvNormal
     @test size(μ) == (xlength,batchsize)
     @test size(σ2) == (xlength,batchsize)
 
-    x = rand(Float32, xlength, batchsize) |> gpu
-    z = rand(Float32, zlength, batchsize) |> gpu
-    loss() = sum(logpdf(p,x,z))
+    loss() = sum(logpdf(p,X,Z))
     ps = Flux.params(p)
     @test length(ps) == 4
     @test loss() isa Float32
@@ -73,16 +71,14 @@
     d = MvNormal(zeros(Float32,xlength), 1f0)
     p = ConditionalMvNormal(m) |> gpu
 
-    res = condition(p, rand(zlength,batchsize)|>gpu)
+    res = condition(p,Z)
     μ = mean(res)
     σ2 = var(res)
     @test res isa ConditionalDists.BatchScalMvNormal
     @test size(μ) == (xlength,batchsize)
     @test size(σ2) == (xlength,batchsize)
 
-    x = rand(Float32, xlength, batchsize) |> gpu
-    z = rand(Float32, zlength, batchsize) |> gpu
-    loss() = sum(logpdf(p,x,z))
+    loss() = sum(logpdf(p,X,Z))
     ps = Flux.params(p)
     @test length(ps) == 2
     @test loss() isa Float32
@@ -90,4 +86,57 @@
 
     f() = sum(rand(p,z))
     @test_nowarn Flux.gradient(f, ps)
+
+
+    # Fixed scalar variance
+    m = Dense(zlength,xlength)
+    p = ConditionalMvNormal(SplitLayer(m,_->2)) |> gpu
+
+    res = condition(p,Z)
+    μ = mean(res)
+    σ2 = var(res)
+    @test res isa ConditionalDists.BatchScalMvNormal
+    @test size(μ) == (xlength,batchsize)
+    @test size(σ2) == (xlength,batchsize)
+
+    loss() = sum(logpdf(p,X,Z))
+    ps = Flux.params(p)
+    @test length(ps) == 2
+    @test loss() isa Float32
+    @test_nowarn gs = Flux.gradient(loss, ps)
+
+    f() = sum(rand(p,z))
+    @test_nowarn Flux.gradient(f, ps)
+
+
+    # Shared scalar variance TODO: add case for vector shared variance
+    m = Dense(zlength,xlength)
+    σ = ones(Float32,xlength) .* 2
+    σ = 2f0
+    p = ConditionalMvNormal(SplitLayer(m,σ)) |> gpu
+
+    #x = rand(Float32,zlength,10)
+    #display(p.mapping(x))
+    #display(condition(p,x))
+    #display(var(condition(p,x)))
+    #display((σ .^2) .* ones(xlength,batchsize))
+    #error()
+
+    res = condition(p,Z)
+    μ = mean(res)
+    σ2 = var(res)
+    @test all(σ2 .== σ .^2)
+    @test res isa ConditionalDists.BatchScalMvNormal
+    @test size(μ) == (xlength,batchsize)
+    @test size(σ2) == (xlength,batchsize)
+
+    loss() = sum(logpdf(p,X,Z))
+    ps = Flux.params(p)
+    @test length(ps) == 3
+    @test loss() isa Float32
+    @test_nowarn gs = Flux.gradient(loss, ps)
+
+    f() = sum(rand(p,z))
+    @test_nowarn Flux.gradient(f, ps)
+
 end
