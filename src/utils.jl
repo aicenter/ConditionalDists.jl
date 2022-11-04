6473890311d7e412149caac518d4a223212947ba
Vít Škvára <vit.skvara@gmail.com>
2022-11-04 15:50:04 +0100

    updated Flux dependency, fixed SplitLayers

--- src/utils.jl
+++ src/utils.jl
@@ -41,6 +41,15 @@ end
 
 SplitLayer(layers...) = SplitLayer(map(maybe_trainable, layers))
 
+function SplitLayer(in::Int, outs::Vector{Int}, act=identity)
+    acts = [act for _ in 1:length(outs)]
+    SplitLayer(in, outs, acts)
+end
+
+function SplitLayer(in::Int, outs::Vector{Int}, acts::Vector)
+    SplitLayer(Tuple(Dense(in,o,a) for (o,a) in zip(outs,acts)))
+end
+
 function (m::SplitLayer)(x)
     Tuple(layer(x) for layer in m.layers)
 end
